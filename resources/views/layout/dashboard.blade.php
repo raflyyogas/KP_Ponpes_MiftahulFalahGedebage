cd8627c5cdcf05ff9c3efd86f05fa3d8aceca396
Alfian-Rifki <alfianrifki20@gmail.com>
2022-06-11 20:38:52 +0700

    atikel database migration

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -16,9 +16,12 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <!-- Custom styles for this template-->
     <link href="{{ asset('/dashboard/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <script src="https://cdn.ckeditor.com/ckeditor5/34.1.0/classic/ckeditor.js"></script>
+
 
 </head>
 
--- resources/views/tampilan/addartikel.blade.php
+++ resources/views/tampilan/addartikel.blade.php
@@ -10,7 +10,63 @@
             <h6 class="m-0 font-weight-bold text-primary">Artikel</h6>
         </div>
         <div class="card-body">
-            test
+            <form action="" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="row mb-3">
+                    <div class="col-md-2">
+                        <label for="judul" class="form-label">Judul Artikel</label>
+                    </div>
+                    <div class="col">
+                        <input type="text" name="judul" id="judul" class="form-control">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-2">
+                        <label for="foto" class="form-label">Foto Artikel</label>
+                    </div>
+                    <div class="col">
+                        <input type="file" name="foto" id="foto" class="form-control" onchange="previewImg()">
+                        <div class="d-flex justify-content-center mt-2">
+                            <img id="preview" style="max-width:50%">
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-2">
+                        <label for="editor">Isi Artikel</label>
+                    </div>
+                    <div class="col">
+                        <textarea id="editor" rows="5"></textarea>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+
+
+    <script>
+        function previewImg() {
+            const image = document.querySelector('#foto');
+            const preview = document.querySelector('#preview');
+
+            preview.style.display = 'block';
+
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+
+            oFReader.onload = function(oFREvent) {
+                preview.src = oFREvent.target.result;
+            }
+
+
+        }
+        
+        ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+    
 @endsection
